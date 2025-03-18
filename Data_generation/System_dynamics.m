635df1e455a47d0fbcd9d417167431f7dc5257bf
Ole Uphaus <ole.uphaus@gmx.de>
2025-03-18 09:50:32 +0100

    Symbolische Lösung der Lagrange Gleichungen 2. Art hinzugefügt, die auf die nichtlineare Zustandsraumdarstellung führt.

--- Data_generation/System_dynamics.m
+++ Data_generation/System_dynamics.m
@@ -6,6 +6,26 @@
 % mithilfe der Lagrangeschen Gleichungen 2.Art herleiten. Außerdem soll
 % eine Zustandsraumdarstellung hergeleitet werden, die säter zur Simulation
 % verwendet werden kann. Damit nicht in jedem Simulationsschritt die
-% Lagrange Gleichungen gelöst werden müssen, wird dies in diesem Skript
-% getan.
+% Lagrange Gleichungen gelöst werden müssen, wird die Lösung in diesem
+% Skript durchgeführt.
 % -------------------------------------------------------------
+
+% Symbolische variablen definieren
+syms r phi r_p phi_p r_pp phi_pp F tau
+syms J m mL l
+
+% Lagrange Funktion aufstellen
+U = 0;
+T = 1/2*J*phi_p^2 + 1/2*(m + mL)*r_p^2 + 1/2*(m*(r - l)^2)*phi_p^2 + 1/2*(mL*r^2)*phi_p^2;
+L = T - U;
+
+% Lagrange Gleichungen 2.Art
+q = [r; phi];
+q_p = [r_p; phi_p];
+jac_1 = jacobian(L, q_p).';
+jac_2 = jacobian(L, q).';
+
+Lagr_eq = simplify(diff(jac_1, r)*r_p + diff(jac_1, r_p)*r_pp + diff(jac_1, phi)*phi_p + diff(jac_1, phi_p)*phi_pp - jac_2);
+
+% Zustandsraumdarstellung
+x_p = [q_p; solve(Lagr_eq(1) == F, r_pp); solve(Lagr_eq(2) == tau, phi_pp)]
