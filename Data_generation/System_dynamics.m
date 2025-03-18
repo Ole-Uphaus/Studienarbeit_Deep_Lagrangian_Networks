5aa7a47b1b16a5a1eea9193b8d2810f565666905
Ole Uphaus <ole.uphaus@gmx.de>
2025-03-18 09:05:34 +0100

    Skript Initialisiert und Beschreibung hinzugefügt.

---
+++ Data_generation/System_dynamics.m
@@ -0,0 +1,11 @@
+% -------------------------------------------------------------
+% Autor:      Ole Uphaus
+% Datum:      18.03.2025
+% Beschreibung:
+% Dieses Skript soll die differentialgleichungen des 2-FHG Roboters
+% mithilfe der Lagrangeschen Gleichungen 2.Art herleiten. Außerdem soll
+% eine Zustandsraumdarstellung hergeleitet werden, die säter zur Simulation
+% verwendet werden kann. Damit nicht in jedem Simulationsschritt die
+% Lagrange Gleichungen gelöst werden müssen, wird dies in diesem Skript
+% getan.
+% -------------------------------------------------------------
