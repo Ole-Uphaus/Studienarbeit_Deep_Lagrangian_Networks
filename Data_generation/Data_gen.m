1084c9c0e8fc7c7fdf7e4d2f02be62b7003308d2
Ole Uphaus <ole.uphaus@gmx.de>
2025-03-18 14:41:14 +0100

    Mehrere Simulationen für verschiedene Stellgrößenverläufe (jedoch noch konstante Anfangsbedingungen). Am Ende werden für alle Simulationen Plots erstellt.

--- Data_generation/Data_gen.m
+++ Data_generation/Data_gen.m
@@ -62,42 +62,66 @@ x_0 = [r_0; phi_0; r_p_0; phi_p_0]; % Vektor der Anfangswerte
 % Zeitsignal
 t_u = linspace(t_span(1), t_span(2), 1000);
 
-% Funktionsverläufe
-u_zero = t_u.*0;    % kein Eingang
-u_step = heaviside(t_u - 2);    % Sprungfunktion
-u_sin = sin(t_u);   % Sinus Funktion
-u_rectangle = square(2*pi*0.5*t_u);   % Rechteckfunktion
-u_sawtooth = sawtooth(2*pi*0.5*t_u);  % Sägzahnfunktion
+% Eingangssignale Verläufe
+uF_vec = create_random_input(t_u);
+utau_vec = create_random_input(t_u);
 
-u_vec = create_random_input(t_u);
+%% Lösung der ODE mit variablen Stellgrößenverläufen und Eingangssignalen
 
-% Stellgrößen
-F_vec = [t_u; u_sawtooth];
-tau_vec = [t_u; u_step];
+% Anzahl der Stellgrößenverläufe in u
+n = size(uF_vec);
+n = n(1);
 
-%% Differentialgleichung lösen
+% Strukturarray zur Speicherung von Daten
+simData(n^2) = struct();
+counter = 1;
 
-% ODE-Funktion mit Parametern
-odefun = @(t, x) ODE_2_FHG_Robot(t, x, F_vec, tau_vec, l_m, m_kg, mL_kg, J_kgm2);
+% Alle Stellgrößenverläufe durchgehen
+for i = 1:n
+    for j = 1:n
+        % Stellgrößen
+        F_vec = [t_u; uF_vec(i, :)];
+        tau_vec = [t_u; utau_vec(j, :)];
 
-% Solver zur Lösung der DGL
-options = odeset('MaxStep', 0.01, 'Stats', 'on');
-[t, x] = ode45(odefun, t_span, x_0, options);
+        % ODE-Funktion mit Parametern
+        odefun = @(t, x) ODE_2_FHG_Robot(t, x, F_vec, tau_vec, l_m, m_kg, mL_kg, J_kgm2);
+        
+        % Solver zur Lösung der DGL
+        options = odeset('MaxStep', 0.01, 'Stats', 'on');
+        [t, x] = ode45(odefun, t_span, x_0, options);
+
+        % Speichern
+        simData(counter).t = t;
+        simData(counter).x = x;
+
+        counter = counter + 1;
+
+    end
+end
 
 %% Plots erstellen
 
-% Plot 1. Signal in Figure 1
-figure(1);                   % Neues Fenster (Figure 1)
-plot(t, x(:, 1), 'b', 'LineWidth', 1.5);
-xlabel('Zeit [s]');
-ylabel('Weg [m]');
-title('Position r(t)');
-grid on;
+% sollen Plots angezeigt werden?
+showplots = true;
 
-% Plot 2. Signal in Figure 2
-figure(2);                   % Neues Fenster (Figure 2)
-plot(t, x(:, 2), 'r', 'LineWidth', 1.5);
-xlabel('Zeit [s]');
-ylabel('Winkel [rad]');
-title('Winkel phi(t)');
-grid on;
+if showplots == true
+    for i = 1:n^2
+        % Plot erstellen
+        figure(i);
+    
+        % Oberer Plot
+        subplot(2,1,1); % 2 Zeilen, 1 Spalte, oberer Plot
+        plot(simData(i).t, simData(i).x(:, 1), 'b', 'LineWidth', 1.5);
+        xlabel('Zeit [s]');
+        ylabel('Weg [m]');
+        title('Position r(t)');
+        
+        % Unterer Plot
+        subplot(2,1,2); % 2 Zeilen, 1 Spalte, unterer Plot
+        plot(simData(i).t, simData(i).x(:, 2), 'r', 'LineWidth', 1.5);
+        xlabel('Zeit [s]');
+        ylabel('Winkel [rad]');
+        title('Winkel phi(t)');
+    
+    end
+end
