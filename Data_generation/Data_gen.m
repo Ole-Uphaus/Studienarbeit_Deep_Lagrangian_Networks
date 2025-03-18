eda36ae216c71a942dd4106757bf58f3e8a311a2
Ole Uphaus <ole.uphaus@gmx.de>
2025-03-18 11:32:16 +0100

    Skript zur Lösung der DGL hinzugefügt. Aktuell wird nur eine einzelne Simulation durchgeführt und es werden auch keine Daten gespeichert.

---
+++ Data_generation/Data_gen.m
@@ -0,0 +1,57 @@
+% -------------------------------------------------------------
+% Autor:      Ole Uphaus
+% Datum:      18.03.2025
+% Beschreibung:
+% Diese Funktion führt die Generierung und Spiecherung der Trainigsdaten
+% durch. Dabei wird die bereits hergeleitete Zustandsraumdarstellung des
+% 2-FHG Roboters verwendet. 
+% -------------------------------------------------------------
+clc
+clear
+
+%% Definition der Systemparameter und Initialisierung
+% Systemparameter
+m_kg = 5;   % Masse des Arms
+mL_kg = 2;  % Masse der Last
+J_kgm2 = 0.4;  % gesamte Rotationsträgheit
+l_m = 0.25; % Schwerpunktsabstand (Arm - Last)
+
+% Anfangswerte und Simulationsteit
+t_span = [0 10];    % Simulationszeit
+
+r_0 = 0.5;
+phi_0 = 2;
+r_p_0 = 0;
+phi_p_0 = 0;
+x_0 = [r_0; phi_0; r_p_0; phi_p_0]; % Vektor der Anfangswerte
+
+% Eingangssignale 
+t_u = linspace(t_span(1), t_span(2), 1000);
+F_vec = [t_u; 0.*t_u];
+tau_vec = [t_u; 0.*t_u];
+
+% ODE-Funktion mit Parametern
+odefun = @(t, x) ODE_2_FHG_Robot(t, x, F_vec, tau_vec, l_m, m_kg, mL_kg, J_kgm2);
+
+%% Differentialgleichung lösen
+% Solver zur Lösung der DGL
+options = odeset('MaxStep', 0.01, 'Stats', 'on');
+[t, x] = ode45(odefun, t_span, x_0, options);
+
+%% Plots erstellen
+
+% Plot 1. Signal in Figure 1
+figure(1);                   % Neues Fenster (Figure 1)
+plot(t, x(:, 1), 'b', 'LineWidth', 1.5);
+xlabel('Zeit [s]');
+ylabel('Weg [m]');
+title('Position r(t)');
+grid on;
+
+% Plot 2. Signal in Figure 2
+figure(2);                   % Neues Fenster (Figure 2)
+plot(t, x(:, 2), 'r', 'LineWidth', 1.5);
+xlabel('Zeit [s]');
+ylabel('Winkel [rad]');
+title('Winkel phi(t)');
+grid on;
