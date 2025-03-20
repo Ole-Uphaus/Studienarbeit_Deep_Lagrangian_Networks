de54548b73365453d1a34620295ca6ea23fc51c5
Ole Uphaus <ole.uphaus@gmx.de>
2025-03-20 15:11:27 +0100

    Erstellung und Initialisierung eines neuronalen Netzes

--- Training_Models/Feedforward_NN/Feed_forward_NN_pytorch.py
+++ Training_Models/Feedforward_NN/Feed_forward_NN_pytorch.py
@@ -7,6 +7,9 @@ Dieses Skript soll auf Grundlage der Simuleirten Trainingsdaten ein neuronales N
 
 import scipy.io
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
 import os
 
 def extract_training_data(file_name):
@@ -27,7 +30,40 @@ def extract_training_data(file_name):
 
     return features, labels
 
+# Erstellung neuronales Netz (Klasse)
+class Feed_forward_NN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Feed_forward_NN, self).__init__()
+
+        # Hier eine Vereinfachung, dass der befehl in forward Funktion kürzer wird
+        self.net = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, output_size)
+        )
+    
+    def forward(self, x):
+        return self.net(x)
+
+
 # Trainingsdaten laden
 features, labels = extract_training_data('SimData__2025_03_20_13_59_55.mat')
+
+# Trainingsdaten in Torch-Tensoren umwandeln
+features_tensor = torch.tensor(features, dtype=torch.float32)
+labels_tensor = torch.tensor(labels, dtype=torch.float32)
+
+# Dataset und Dataloader erstellen
+dataset = TensorDataset(features_tensor, labels_tensor)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+# Neuronales Netz initialisieren
+input_size = features.shape[1]
+output_size = labels.shape[1]
+hidden_size = 10
+
+model = Feed_forward_NN(input_size, hidden_size, output_size)
 
 
