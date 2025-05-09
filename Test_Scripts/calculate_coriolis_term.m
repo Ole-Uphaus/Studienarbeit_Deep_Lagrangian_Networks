777a9e3dc4ee5bad6eb7f5e4468c30a5da6eafb0
Ole Uphaus <ole.uphaus@gmx.de>
2025-05-09 17:16:40 +0200

    Meine Bisheriege Implementierung nochmal getestet und festgestellt, dass ich im aktuellen Modell über die falschen Tensorrichtungen summiere.

--- Test_Scripts/calculate_coriolis_term.m
+++ Test_Scripts/calculate_coriolis_term.m
@@ -25,7 +25,7 @@ q_p = [q1_p;
 
 % Matrix H(q) definieren
 H = [2*q1, q2^2;
-    q2^2, q1*q2];
+    q2^2, q1*q2]
 
 %% 1. Moglichkeit: Ausmultiplizieren und am Ende ableiten
 
@@ -33,15 +33,45 @@ H = [2*q1, q2^2;
 qpT_H_qp = q_p'*H*q_p;   % Skalerer Ausdruck
 
 % Ableitung nach q bilden
-result_1 = jacobian(qpT_H_qp, q);    % Zeilenvektor
+result_1 = jacobian(qpT_H_qp, q)    % Zeilenvektor
 
 %% 2. Möglichkeit: Zuerst dh/dq berechnen und dann ausmultiplizieren
 
 % dH/dq berechnen
-dH_dq1 = diff(H, q1)    % Ableitungen einzeln berechnen , da jacobian keine Matrizen als eingang erlaubt
-dH_dq2 = diff(H, q2)
+dH_dq1 = diff(H, q1);    % Ableitungen einzeln berechnen , da jacobian keine Matrizen als eingang erlaubt
+dH_dq2 = diff(H, q2);
 
-dH_dq = cat(3, dH_dq1, dH_dq2)  % Einzelne Ableitung zu 3D Matrix (Würfel zusammensetzen)
+dH_dq = cat(3, dH_dq1, dH_dq2);  % Einzelne Ableitung zu 3D Matrix (Würfel zusammensetzen)
 
-% Ausmultiplizieren dH/dq*qp
-dH1dq_qp = reshape(dH_dq(1, :, :), 2, 2)
+% Ausmultiplizieren qpT*dH/dq*qp
+dHdq1_qp = q_p'*dH_dq(:, :, 1)*q_p;
+dHdq2_qp = q_p'*dH_dq(:, :, 2)*q_p;
+
+result_2 = [dHdq1_qp, dHdq2_qp]
+
+%% Möglichkeit 3: Zuerst dh/dq berechnen und dann ausmultiplizieren (so, wie ich es bis jetzt implementiert habe)
+
+% Ausmultiplizieren dH/dq*qp (in dq Richtung - letzte richtung)
+dH1dq_qp = reshape(dH_dq(1, :, :), 2, 2)*q_p;
+dH2dq_qp = reshape(dH_dq(2, :, :), 2, 2)*q_p;
+
+dHdq_qp = [dH1dq_qp'; dH2dq_qp'];
+
+% Ausmultiplizieren qpT*dH/dq*qp
+result_3 = q_p'*dHdq_qp
+
+%% Übereinstimmung prüfen
+
+% Ergebnisse 1 und 2
+if isequal(result_1, result_2)
+    disp('Ergebnisse 1 und 2 stimmen überein.')
+else
+    disp('Ergebnisse 1 und 2 stimmen nicht überein.')
+end
+
+% Ergebnisse 1 und 3
+if isequal(result_1, result_3)
+    disp('Ergebnisse 1 und 3 stimmen überein.')
+else
+    disp('Ergebnisse 1 und 3 stimmen nicht überein.')
+end
