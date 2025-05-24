bb2b795023337a36963fda352987c8b1bde2c5d8
Ole Uphaus <ole.uphaus@gmx.de>
2025-05-24 14:52:00 +0200

    Erstellung eines python Skriptes um das xml modell zu evaluieren.

---
+++ Mujoco/Model_evauation/forward_dyn.py
@@ -0,0 +1,9 @@
+'''
+Autor:      Ole Uphaus
+Datum:     24.05.2025
+Beschreibung:
+In diesem Skript möchte ich die Mujoco Python Schnittstelle ausprobieren. Dazu werde ich versuchen die inverse Dynamik meines Modells auszuwerten und diese mit dem selbst hergeleiteten analytischen Modell zu vergleichen.
+'''
+
+import mujoco
+import numpy as np
