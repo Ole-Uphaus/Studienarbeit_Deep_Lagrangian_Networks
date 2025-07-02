522f1f4a4ae31d0be62f100c06873abb34dc335e
Ole Uphaus <ole.uphaus@gmx.de>
2025-07-02 14:29:41 +0200

    Reibungsmodell in Torsionsschwinger eingebaut. Generierung neuer Trainingsdaten.

--- Data_generation/Data_gen_Torsionsschwinger_friction.m
+++ Data_generation/Data_gen_Torsionsschwinger_friction.m
@@ -1,12 +1,10 @@
 % -------------------------------------------------------------
 % Autor:      Ole Uphaus
-% Datum:      01.07.2025
+% Datum:      02.07.2025
 % Beschreibung:
-% In diesem Skript werde ich auf Basis des Torsionsschwingermodells
-% Trainingsdaten für das DeLaN Modell generieren. Dazu werde ich zunächst
-% trajektorien für eine Scheibe (1 FHG System) generieren und die
-% benötigten Drehmomente berechnen. Anschließend werde ich mithilfe der
-% berechneten Drehmomente eine Simulation der 2 FHG-Systems durchführen.
+% Dies ist eine Erweiterung des Skriptes zur Erstellung von Trainingsdaten
+% für den Torsionsschwinger. Es wird zusätzlich eine Stribeck Reibung
+% berücksichtigt, die gleichermaßen auf beide Scheiben einwirkt.
 % -------------------------------------------------------------
 
 clc
@@ -49,18 +47,24 @@ end
 % Diese Funktion wertet die inverse Dynamik eines einfachen 1 FHG Systems
 % aus, um gute Drehmomentverläufe zu generieren. Diese Drehmomentverläufe
 % werden später zur Simulation verwendet.
-function tau = inv_dyn_1_FHG(traj_data)
+function tau = inv_dyn_1_FHG(traj_data, d_Ns, tau_c_Nm, tau_s_Nm, nu_ps, epsilon)
 
     % Systemparameter
     J_kgm2 = 0.06;  % hier etwa J1 + J2, damit der Motor beide beschleunigt
 
+    q1_p = traj_data.q1_p;
+    q1_pp = traj_data.q1_pp;
+
+    % reibungskräfte berechnen (jeweils mit doppelten Geschwindigkeiten)
+    tau_f = (2*tau_c_Nm + 2*tau_s_Nm .* exp(-q1_p.^2 ./ nu_ps)) .* tanh(epsilon.*q1_p) + 2*d_Ns .* q1_p;
+
     % Auswertung inverse Dynamik
-    tau = J_kgm2.*traj_data.q1_pp;
+    tau = J_kgm2.*q1_pp + tau_f;
 
 end
 
 % Dies ist die Zustandsraumdarstellung des Torsionsschwingers
-function x_p = ZRD_2_FHG(t, x, tau_vec, t_vec, J_1_kgm2, J_2_kgm2, c_phi_Nmprad)
+function x_p = ZRD_2_FHG(t, x, tau_vec, t_vec, J_1_kgm2, J_2_kgm2, c_phi_Nmprad, d_Ns, tau_c_Nm, tau_s_Nm, nu_ps, epsilon)
 
     % Drehmoment interpolieren
     tau = interp1(t_vec, tau_vec, t);
@@ -78,7 +82,10 @@ function x_p = ZRD_2_FHG(t, x, tau_vec, t_vec, J_1_kgm2, J_2_kgm2, c_phi_Nmprad)
     K = [c_phi_Nmprad -c_phi_Nmprad;
         -c_phi_Nmprad c_phi_Nmprad];
 
-    q_pp = inv(M)*([tau; 0] - K*q);
+    tau_f = [(tau_c_Nm + tau_s_Nm .* exp(-q_p(1).^2 ./ nu_ps)) .* tanh(epsilon.*q_p(1)) + d_Ns .* q_p(1);
+        (tau_c_Nm + tau_s_Nm .* exp(-q_p(2).^2 ./ nu_ps)) .* tanh(epsilon.*q_p(2)) + d_Ns .* q_p(2)];
+
+    q_pp = inv(M)*([tau; 0] - K*q - tau_f);
     
     % Ableitung Zustandsvektor berechnen
     x_p = [q_p;
@@ -97,6 +104,13 @@ t_vec = linspace(0, move_time, samples_per_run);
 J_1_kgm2 = 0.029;
 J_2_kgm2 = 0.029;
 c_phi_Nmprad = 7.309;
+
+d_Ns = 0.02;    % Dämpung
+tau_c_Nm = 0.3;   % Coulomb-Reibkraft
+tau_s_Nm = 0.15;   % zusätzlicher statischer Anteil
+nu_ps = 0.3;
+
+epsilon = 100;  % Parameter zur Regularisierung mit tanh()
 
 % Zeitpunkte der Wegpunkte
 waypointTimes = [0 move_time];
@@ -143,7 +157,7 @@ end
 for i = 1:number_runs
 
     % inverse Dynamik auswerten
-    traj_data(i).tau = inv_dyn_1_FHG(traj_data(i));
+    traj_data(i).tau = inv_dyn_1_FHG(traj_data(i), d_Ns, tau_c_Nm, tau_s_Nm, nu_ps, epsilon);
 
 end
 
@@ -164,7 +178,7 @@ for i = 1:number_runs
     tau_vec = traj_data(i).tau;
 
     % ODE-Funktion mit Parametern
-    odefun = @(t, x) ZRD_2_FHG(t, x, tau_vec, t_vec, J_1_kgm2, J_2_kgm2, c_phi_Nmprad);
+    odefun = @(t, x) ZRD_2_FHG(t, x, tau_vec, t_vec, J_1_kgm2, J_2_kgm2, c_phi_Nmprad, d_Ns, tau_c_Nm, tau_s_Nm, nu_ps, epsilon);
 
     % Solver zur Lösung der DGL
     options = odeset('Stats', 'on');
@@ -180,7 +194,7 @@ for i = 1:number_runs
         x_j = x(j, :);
 
         % ZRD auswerten
-        x_p_j = ZRD_2_FHG(t_vec(j), x_j, tau_vec, t_vec, J_1_kgm2, J_2_kgm2, c_phi_Nmprad);
+        x_p_j = ZRD_2_FHG(t_vec(j), x_j, tau_vec, t_vec, J_1_kgm2, J_2_kgm2, c_phi_Nmprad, d_Ns, tau_c_Nm, tau_s_Nm, nu_ps, epsilon);
 
         % Federkräfte Berechnen
         K = [c_phi_Nmprad -c_phi_Nmprad;
@@ -193,6 +207,9 @@ for i = 1:number_runs
         g(j, :) = g_j;
 
     end
+
+    % Reibkräfte berechnen
+    tau_f = [(tau_c_Nm + tau_s_Nm .* exp(-x(:, 3).^2 ./ nu_ps)) .* tanh(epsilon.*x(:, 3)) + d_Ns .* x(:, 3), (tau_c_Nm + tau_s_Nm .* exp(-x(:, 4).^2 ./ nu_ps)) .* tanh(epsilon.*x(:, 4)) + d_Ns .* x(:, 4)];
 
     % Speichern
     sim_data(i).q1 = x(:, 1);
@@ -215,6 +232,9 @@ for i = 1:number_runs
     sim_data(i).g_1 = g(:, 1);
     sim_data(i).g_2 = g(:, 2);
 
+    sim_data(i).tau_f_1 = tau_f(:, 1);
+    sim_data(i).tau_f_2 = tau_f(:, 2);
+
 end
 
 %% Simulationsergebnisse Speichern (Daten in Trainings- und Testdaten aufteilen)
@@ -225,7 +245,7 @@ test_idx = randperm((number_runs), number_testdata);   % Zufällige Indizees fü
 
 features_test = [sim_data(test_idx(1)).q1, sim_data(test_idx(1)).q2, sim_data(test_idx(1)).q1_p, sim_data(test_idx(1)).q2_p, sim_data(test_idx(1)).tau_1, sim_data(test_idx(1)).tau_2];
 labels_test = [sim_data(test_idx(1)).q1_pp, sim_data(test_idx(1)).q2_pp];
-Mass_Cor_test = [sim_data(test_idx(1)).M_11, sim_data(test_idx(1)).M_12, sim_data(test_idx(1)).M_22, sim_data(test_idx(1)).C_1, sim_data(test_idx(1)).C_2, sim_data(test_idx(1)).g_1, sim_data(test_idx(1)).g_2];
+Mass_Cor_test = [sim_data(test_idx(1)).M_11, sim_data(test_idx(1)).M_12, sim_data(test_idx(1)).M_22, sim_data(test_idx(1)).C_1, sim_data(test_idx(1)).C_2, sim_data(test_idx(1)).g_1, sim_data(test_idx(1)).g_2, sim_data(test_idx(1)).tau_f_1, sim_data(test_idx(1)).tau_f_2];
 
 for i = test_idx(2:end)
     % Features [q1, q2, q1_p, q2_p, tau_1, tau_2]
@@ -234,9 +254,9 @@ for i = test_idx(2:end)
     % Labels [q1_pp, q2_pp]
     labels_test = [labels_test;
         sim_data(i).q1_pp, sim_data(i).q2_pp];
-    % Mass and Coriolis Terms [M_11, M_12, M_22, C_1, C_2, g_1, g_2]
+    % Mass and Coriolis Terms [M_11, M_12, M_22, C_1, C_2, g_1, g_2, fric_1, fric_2]
     Mass_Cor_test = [Mass_Cor_test;
-        sim_data(i).M_11, sim_data(i).M_12, sim_data(i).M_22, sim_data(i).C_1, sim_data(i).C_2, sim_data(i).g_1, sim_data(i).g_2];
+        sim_data(i).M_11, sim_data(i).M_12, sim_data(i).M_22, sim_data(i).C_1, sim_data(i).C_2, sim_data(i).g_1, sim_data(i).g_2, sim_data(i).tau_f_1, sim_data(i).tau_f_2];
 end
 
 % Trainingsdaten auswählen und in Features und Labels aufteilen (80%)
@@ -244,7 +264,7 @@ training_idx = setdiff((1:number_runs), test_idx);
 
 features_training = [sim_data(training_idx(1)).q1, sim_data(training_idx(1)).q2, sim_data(training_idx(1)).q1_p, sim_data(training_idx(1)).q2_p, sim_data(training_idx(1)).tau_1, sim_data(training_idx(1)).tau_2];
 labels_training = [sim_data(training_idx(1)).q1_pp, sim_data(training_idx(1)).q2_pp];
-Mass_Cor_training = [sim_data(training_idx(1)).M_11, sim_data(training_idx(1)).M_12, sim_data(training_idx(1)).M_22, sim_data(training_idx(1)).C_1, sim_data(training_idx(1)).C_2, sim_data(training_idx(1)).g_1, sim_data(training_idx(1)).g_2];
+Mass_Cor_training = [sim_data(training_idx(1)).M_11, sim_data(training_idx(1)).M_12, sim_data(training_idx(1)).M_22, sim_data(training_idx(1)).C_1, sim_data(training_idx(1)).C_2, sim_data(training_idx(1)).g_1, sim_data(training_idx(1)).g_2, sim_data(training_idx(1)).tau_f_1, sim_data(training_idx(1)).tau_f_2];
 
 for i = training_idx(2:end)
     % Features [q1, q2, q1_p, q2_p, tau_1, tau_2]
@@ -253,9 +273,9 @@ for i = training_idx(2:end)
     % Labels [q1_pp, q2_pp]
     labels_training = [labels_training;
         sim_data(i).q1_pp, sim_data(i).q2_pp];
-    % Mass and Coriolis Terms [M_11, M_12, M_22, C_1, C_2, g_1, g_2]
+    % Mass and Coriolis Terms [M_11, M_12, M_22, C_1, C_2, g_1, g_2, fric_1, fric_2]
     Mass_Cor_training = [Mass_Cor_training;
-        sim_data(i).M_11, sim_data(i).M_12, sim_data(i).M_22, sim_data(i).C_1, sim_data(i).C_2,sim_data(i).g_1, sim_data(i).g_2];
+        sim_data(i).M_11, sim_data(i).M_12, sim_data(i).M_22, sim_data(i).C_1, sim_data(i).C_2,sim_data(i).g_1, sim_data(i).g_2, sim_data(i).tau_f_1, sim_data(i).tau_f_2];
 end
 
 if savedata == true
@@ -269,7 +289,7 @@ if savedata == true
     % Datei speichern
     num_samples = num2str(length(features_test) + length(features_training));
     time_stamp = string(datetime('now', 'Format', 'yyyy_MM_dd_HH_mm_ss'));
-    dateiName = 'SimData_Torsionsschwinger_' + time_stamp + '_Samples_' + num_samples + '.mat';   
+    dateiName = 'SimData_Torsionsschwinger_friction_' + time_stamp + '_Samples_' + num_samples + '.mat';   
     full_path = fullfile(target_folder, dateiName);
     save(full_path, 'features_training', 'labels_training', 'features_test', 'labels_test', "Mass_Cor_test");
 end
@@ -326,22 +346,36 @@ ylabel('q2pp');
 grid on;
 title('q2pp(t)');
 
-% Plot F, tau
+% Plot F, tau, Dämpfung
 figure('WindowState','maximized');
 
-subplot(2,1,1);
+subplot(2,2,1);
 plot(t_vec_ges, features_training(:, 5), 'b', 'LineWidth', 1.5);
 xlabel('Zeit [s]');
 ylabel('tau_1');
 grid on;
 title('tau1(t)');
 
-subplot(2,1,2);
+subplot(2,2,3);
 plot(t_vec_ges, features_training(:, 6), 'b', 'LineWidth', 1.5);
 xlabel('Zeit [s]');
 ylabel('tau_2');
 grid on;
 title('tau2(t)');
+
+subplot(2,2,2);
+plot(t_vec_ges, Mass_Cor_training(:, 8), 'b', 'LineWidth', 1.5);
+xlabel('Zeit [s]');
+ylabel('fric1');
+grid on;
+title('fric1(t)');
+
+subplot(2,2,4);
+plot(t_vec_ges, Mass_Cor_training(:, 9), 'b', 'LineWidth', 1.5);
+xlabel('Zeit [s]');
+ylabel('fric2');
+grid on;
+title('fric2(t)');
 
 % Plott M_11, M_12, M_22
 figure('WindowState','maximized');
