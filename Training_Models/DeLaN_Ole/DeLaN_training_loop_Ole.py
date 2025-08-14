e5051d14b13905d131f7ade06e3a3849d1034b16
Ole Uphaus <ole.uphaus@gmx.de>
2025-08-14 15:42:58 +0200

    loop skript angepasst - mit trainingsfunktion

--- Training_Models/DeLaN_Ole/DeLaN_training_loop_Ole.py
+++ Training_Models/DeLaN_Ole/DeLaN_training_loop_Ole.py
@@ -8,10 +8,10 @@ In diesem Skript wird das von mir erstellte Deep Lagrangien Network trainiert. H
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 import numpy as np
-import time
+import os
 import matplotlib.pyplot as plt
 
-from DeLaN_model_Ole import Deep_Lagrangian_Network
+from DeLaN_training_Ole import *
 from DeLaN_functions_Ole import *
 
 # Checken, ob Cuda verfügbar und festlegen des devices, auf dem trainiert werden soll
@@ -19,8 +19,49 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Benutze Device: {device}")
 print()
 
-# Seeds ausprobieren
-max_seed = 100
+# Parameter festlegen
+hyper_param = {
+    # Netzparameter
+    'hidden_width': 64,
+    'hidden_depth': 2,
+    'activation_fnc': 'elu',
+    'activation_fnc_diag': 'softplus',
+
+    # Initialisierung
+    'bias_init_constant': 1.e-3,
+    'wheight_init': 'xavier_normal',
+
+    # Lagrange Dynamik
+    'L_diagonal_offset': 1.e-2,
+    
+    # Training
+    'dropuot': 0.0,
+    'batch_size': 512,
+    'learning_rate': 5.e-4,
+    'weight_decay': 1.e-4,
+    'n_epoch': 200,
+
+    # Reibungsmodell
+    'use_friction_model': False,
+    'friction_model_init_d': [0.01, 0.01],
+    'friction_model_init_c': [0.01, 0.01],
+    'friction_model_init_s': [0.01, 0.01],
+    'friction_model_init_v': [0.01, 0.01],
+    'friction_epsilon': 100.0,
+
+    # Sonstiges
+    'use_inverse_model': True,
+    'use_forward_model': True,
+    'use_energy_consumption': False,
+    'save_model': False}
+
+# Trainings- und Testdaten laden
+target_folder = 'Studienarbeit_Data' # Möglichkeiten: 'MATLAB_Simulation', 'Mujoco_Simulation', 'Torsionsschwinger_Messungen' 'Studienarbeit_Data'
+features_training, labels_training, _, _, _ = extract_training_data('Allgemeiner_Trainingsdatensatz_Nruns_37.mat', target_folder)  # Mein Modell Trainingsdaten
+_, _, features_test, labels_test, Mass_Cor_test = extract_training_data('Allgemeiner_Trainingsdatensatz_Nruns_37.mat', target_folder)  # Mein Modell Testdaten (Immer dieselben Testdaten nutzen)
+
+# Seeds festlegen
+max_seed = 3
 seed_vec = np.arange(1, max_seed + 1)
 
 # Ergebnisvektor
@@ -32,158 +73,49 @@ for i_seed in seed_vec:
 
     print('Aktueller Seed: ', i_seed)
 
-    # Seed setzen für Reproduzierbarkeit
+    # Seed setzen
     seed = i_seed
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
-    # Parameter festlegen
-    hyper_param = {
-        # Netzparameter
-        'hidden_width': 64,
-        'hidden_depth': 2,
-        'activation_fnc': 'elu',
-        'activation_fnc_diag': 'softplus',
+    # Modell trainieren
+    DeLaN_network, results = Delan_Train_Eval(
+            target_folder,
+            features_training,
+            labels_training,
+            features_test,
+            labels_test,
+            hyper_param,
+            device
+        )
+    
+    # Ergebnisse entpacken
+    training_loss_history = np.array(results['training_loss_history'])
+    test_loss_history = np.array(results['test_loss_history'])
 
-        # Initialisierung
-        'bias_init_constant': 1.e-3,
-        'wheight_init': 'xavier_normal',
-
-        # Lagrange Dynamik
-        'L_diagonal_offset': 1.e-4,
-        
-        # Training
-        'dropuot': 0.0,
-        'batch_size': 512,
-        'learning_rate': 5.e-4,
-        'weight_decay': 1.e-4,
-        'n_epoch': 3,
-
-        # Reibungsmodell
-        'use_friction_model': False,
-        'friction_model_init_v': 0.01,
-        'friction_epsilon': 100.0,
-
-        # Sonstiges
-        'save_model': False}
-
-    # Trainings- und Testdaten laden
-    target_folder = 'MATLAB_Simulation' # Möglichkeiten: 'MATLAB_Simulation', 'Mujoco_Simulation'
-    features_training, labels_training, _, _, _ = extract_training_data('SimData_V3_Rob_Model_1_2025_05_09_10_27_03_Samples_3000.mat', target_folder)  # Mein Modell Trainingsdaten
-    _, _, features_test, labels_test, Mass_Cor_test = extract_training_data('SimData_V3_Rob_Model_1_2025_05_09_10_27_03_Samples_3000.mat', target_folder)  # Mein Modell Testdaten (Immer dieselben Testdaten nutzen)
-
-    # Torch Tensoren der Trainingsdaten erstellen
-    features_training_tensor = torch.tensor(features_training, dtype=torch.float32)
-    labels_training_tensor = torch.tensor(labels_training, dtype=torch.float32)
-
-    # Dataset und Dataloader für das Training erstellen
-    dataset_training = TensorDataset(features_training_tensor, labels_training_tensor)
-    dataloader_training = DataLoader(dataset_training, batch_size=hyper_param['batch_size'], shuffle=True, drop_last=True)
-
-    # Testdaten in torch Tensoren umwandeln
-    features_test_tensor = torch.tensor(features_test, dtype=torch.float32)
-    labels_test_tensor = torch.tensor(labels_test, dtype=torch.float32)
-
-    # Ausgabe Datendimensionen
-    print('Datenpunkte Training: ', features_training.shape[0])
-    print('Datenpunkte Evaluierung: ', features_test.shape[0])
-    print()
-
-    # DeLaN Netzwerk erstellen
-    n_dof = labels_training.shape[1]
-    DeLaN_network = Deep_Lagrangian_Network(n_dof, **hyper_param).to(device)
-
-    # Optimierer Initialisieren
-    optimizer = torch.optim.Adam(DeLaN_network.parameters(),
-                                    lr=hyper_param["learning_rate"],
-                                    weight_decay=hyper_param["weight_decay"],
-                                    amsgrad=True)
-
-    # Optimierung starten und Zeitmessung beginnen
-    print('Starte Optimierung...')
-    print()
-    start_time = time.time()
-
-    # Training des Netzwerks
-    loss_history = []
-    for epoch in range(hyper_param['n_epoch']):
-        # Modell in den Trainingsmodeus versetzen und loss Summe initialisieren
-        DeLaN_network.train()
-        loss_sum = 0
-
-        for batch_features, batch_labels in dataloader_training:
-            # Gradienten zurücksetzen
-            optimizer.zero_grad()
-
-            # Trainingsdaten zuordnen
-            q = batch_features[:, (0, 1)].to(device)
-            qd = batch_features[:, (2, 3)].to(device)
-            qdd = batch_features[:, (4, 5)].to(device)
-            tau = batch_labels.to(device)
-
-            # Forward pass
-            tau_hat, _, _, _, _ = DeLaN_network(q, qd, qdd)
-
-            # Fehler aus inverser Dynamik berechnen (Schätzung von tau)
-            err_inv_dyn = torch.sum((tau_hat - tau)**2, dim=1)
-            mean_err_inv_dyn = torch.mean(err_inv_dyn)
-
-            # Loss berechnen und Optimierungsschritt durchführen
-            loss = mean_err_inv_dyn
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(DeLaN_network.parameters(), max_norm=0.5)    # Gradienten Clopping für besseres Training
-            optimizer.step()
-
-            # Loss des aktuellen Batches aufsummieren
-            loss_sum += loss.item()
-
-        # Mittleren Loss berechnen und ausgeben
-        loss_mean_batch = loss_sum/len(dataloader_training)
-
-        # Loss an Loss history anhängen
-        loss_history.append([epoch + 1, loss_mean_batch])
-
-        if epoch == 0 or np.mod(epoch + 1, 100) == 0:
-            # Ausgabe während des Trainings
-            print(f'Epoch [{epoch + 1}/{hyper_param['n_epoch']}], Training-Loss: {loss_mean_batch:.3e}, Verstrichene Zeit: {(time.time() - start_time):.2f} s')
-
-    # Modell evaluieren (kein torch.nograd(), da interne Gradienten benötigt werden)
-    DeLaN_network.eval()
-
-    # Testdaten zuordnen und auf device verschieben
-    q_test = features_test_tensor[:, (0, 1)].to(device)
-    qd_test = features_test_tensor[:, (2, 3)].to(device)
-    qdd_test = features_test_tensor[:, (4, 5)].to(device)
-    tau_test = labels_test_tensor.cpu().numpy()    # Diesen Tensor direkt auf cpu schieben, damit damit nachher der loss berechnet werden kann
-
-    # Prädiktion
-    print()
-    print('Evaluierung...')
-
-    # Forward pass
-    out_test = DeLaN_network(q_test, qd_test, qdd_test)
-
-    tau_hat_test = out_test[0].cpu().detach().numpy()   # Tesnoren auf cpu legen, gradienten entfernen, un numpy arrays umwandeln
-
-    # Test loss berechnen (um mit Training zu vergleichen)
-    err_inv_dyn_test = np.sum((tau_hat_test - tau_test)**2, axis=1)
-    mean_err_inv_dyn_test = np.mean(err_inv_dyn_test)
-
-    # Ausgabe loss
-    print(f'Test-Loss: {mean_err_inv_dyn_test:.3e}')
-    print()
+    # Kleinsten evaluierungsfehler finden
+    error_eval = np.min(test_loss_history[:, 1])
+    min_idx = np.argmin(test_loss_history[:, 1])
+    error_train = training_loss_history[min_idx, 1]
 
     # Fehler abspeichern
-    seed_loss_vec[i_seed - 1, 1] = mean_err_inv_dyn_test
-    seed_loss_vec[i_seed - 1, 2] = loss_mean_batch
+    seed_loss_vec[i_seed - 1, 1] = error_train # Fehler Training
+    seed_loss_vec[i_seed - 1, 2] = error_eval # Fehler Evaluierung
 
-print(seed_loss_vec)
+# Ausgabe Fehlervektor
+print('Ergebnisvektor: \n', seed_loss_vec)
+
+# Fehlervektor abspeichern für spätere Plots
+script_path = os.path.dirname(os.path.abspath(__file__))
+save_path = os.path.join(script_path, '..', 'DeLaN_Plots', 'Ergebnisse_Seed_Untersuchung.npy')
+
+np.save(save_path, seed_loss_vec)
 
 # Fehler Visualisieren
 plt.figure()
-plt.scatter(seed_loss_vec[:, 0], seed_loss_vec[:, 1], label='Test-Loss')
-plt.scatter(seed_loss_vec[:, 0], seed_loss_vec[:, 2], label='Training-Loss')
+plt.scatter(seed_loss_vec[:, 0], seed_loss_vec[:, 1], label='Training-Loss')
+plt.scatter(seed_loss_vec[:, 0], seed_loss_vec[:, 2], label='Test-Loss')
 plt.xscale("linear")
 plt.yscale("log") 
 plt.xlabel("Seed")
